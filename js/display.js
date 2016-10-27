b028d68432915747a62af43c8298f41b7741890e
Juliane Blier <blier.juliane@gmail.com>
2016-10-27 12:00:35 +0200

    Réorganisation des fichiers

---
+++ js/display.js
@@ -0,0 +1,50 @@
+(function(){
+	"use strict";
+	var app = {
+		menu: null,
+		article: null,
+		config: {},
+		init:function(){
+			var self = this;
+			this.config = window.appConfig;
+			$('#articles').on('click', 'a', function(event){
+				event.preventDefault();
+				self.getFile($(this).attr('href'));
+				return false;
+			});
+			this.get('/menu.json', this.displayMenu.bind(this));
+		},
+		get: function(path, callback){
+			$.ajax({
+				url: this.config.url + path,
+				type: 'GET',
+				success: callback.bind(this),
+				error: function(){
+					console.log('error JSON');
+				},
+			});
+		},
+		getFile: function(page){
+			this.get(page, this.displayFile.bind(this));
+		},
+		displayFile: function(data){
+			this.article = data;
+			app.transformMd(data);
+		},
+		transformMd: function(text){
+			var converter = new showdown.Converter(),
+			html = converter.makeHtml(text);
+			$('#corpus').html(html);
+		},
+		displayMenu: function(data){
+			app.menu = data.menu;
+			for(var i = 0; i < this.menu.length ; i++){
+				$('#articles').append('<a href="' + this.menu[i].path + '" class="item">' + app.menu[i].title +'</a>');
+			}
+		}
+	};
+
+	$(document).ready(function(){
+		app.init();
+	});
+})();
---
+++ js/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/index.html', function (req, res) {
+  // res.send('Hello World!');
+});
+
+app.listen(2605, function () {
+  console.log('Your app is listening on port 2605!');
+});
+
+app.use(express.static('articles'));
+app.use(express.static('menu'));
+app.use(express.static('html'));
